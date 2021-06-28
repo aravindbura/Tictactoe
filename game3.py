87c101fa90aa136dd9609924371232440a05454e
aravindbura <78644762+aravindbura@users.noreply.github.com>
2021-06-28 22:29:44 +0530

    Update game3.py

--- game3.py
+++ game3.py
@@ -22,9 +22,6 @@ def player_input():
     
     while not (marker == 'X' or marker == 'O'):
         marker = input('Player 1: Do you want to be X or O? ').upper()
-
-        
-        
         
     if marker == 'X':
         return ('X', 'O')
